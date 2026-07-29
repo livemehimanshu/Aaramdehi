334783e85ef5920342229e9ad0d67554c9733f1e
livemehimanshu <46988939+livemehimanshu@users.noreply.github.com>
2026-07-29 17:08:47 +0530

    Updated code

--- vite.config.js
+++ vite.config.js
@@ -32,9 +32,6 @@ export default defineConfig({
       output: {
         manualChunks(id) {
           if (id.includes('node_modules')) {
-            if (id.includes('react/') || id.includes('react-dom/') || id.includes('react-router')) {
-              return 'vendor-react';
-            }
             if (id.includes('firebase')) {
               return 'vendor-firebase';
             }
@@ -50,10 +47,6 @@ export default defineConfig({
             if (id.includes('@mui') || id.includes('@emotion')) {
               return 'vendor-mui';
             }
-            if (id.includes('lucide-react')) {
-              return 'vendor-icons';
-            }
-            return 'vendor-others';
           }
         },
       },
